e5979de2ec50a145d0994014212b2b75ad741132
Drehon <filippo.monti66@gmail.com>
2025-07-24 19:11:08 +0200

    lista non carica attempt fix 2

--- forge.config.js
+++ forge.config.js
@@ -4,6 +4,11 @@ const { FuseV1Options, FuseVersion } = require('@electron/fuses');
 module.exports = {
   packagerConfig: {
     asar: true,
+    webpack: {
+      externals: {
+        path: 'require("path")',
+      },
+    },
   },
   rebuildConfig: {},
   makers: [
