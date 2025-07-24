8eadf7f4b603459b236cd6de4f3ef2d4f984ed6f
Drehon <filippo.monti66@gmail.com>
2025-07-24 20:58:52 +0200

    created test_notes2.txt, app works but doesn't locate anything

--- src/main.js
+++ src/main.js
@@ -21,13 +21,8 @@ const createWindow = () => {
     },
   });
 
-  // Carica il file index.html (la nostra schermata Home).
-  // VITE_DEV_SERVER_URL è una variabile speciale per l'ambiente di sviluppo.
-  if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
-    mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
-  } else {
-    mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`));
-  }
+  // and load the index.html of the app.
+  mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
 
   // Apri i DevTools in sviluppo.
   if (process.env.NODE_ENV === 'development') {
