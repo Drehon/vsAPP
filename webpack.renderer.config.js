224214376575bf4befb53b30adc99d514d4d19cb
Drehon <filippo.monti66@gmail.com>
2025-07-25 12:49:29 +0200

    trying to fix console errors

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -1,17 +1,13 @@
 const rules = require('./webpack.rules');
 
+rules.push({
+  test: /\.css$/,
+  use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+});
+
 module.exports = {
   // Put your normal webpack config below here
   module: {
     rules,
-  },
-  externals: {
-    // In order to work with electron-forge, we need to tell webpack not to bundle node modules
-    // so that we can use them in the renderer process.
-    // See: https://webpack.js.org/configuration/externals/
-    //
-    // In this case, we are telling webpack to not bundle the 'fs' and 'path' modules.
-    // We can add more modules here if we need them.
-    ...require('webpack-node-externals')(),
   },
 };
