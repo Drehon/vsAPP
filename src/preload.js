d8ac3144b55db10dd82686a51bac5e7a328b7771
Drehon <filippo.monti66@gmail.com>
2025-07-25 21:10:28 +0200

    a

--- src/preload.js
+++ src/preload.js
@@ -1,14 +1,14 @@
 const { ipcRenderer } = require('electron');
 
 // With contextIsolation disabled, we attach the API directly to the window object.
-// This is less secure but required by the project's build tooling.
+// This is less secure but is required by the project's build tooling.
 window.api = {
   getAppPath: () => ipcRenderer.invoke('get-app-path'),
   navigateTo: (relativePath) => {
     ipcRenderer.send('navigate', relativePath);
   },
   getLessons: () => ipcRenderer.invoke('get-lessons'),
-  getExercises: () => ipcRenderer.invoke('get-exercises'),
+  getExercises: () => ipcRenderer.invoke('get-exercises'), 
   saveProgress: (lesson, data) => ipcRenderer.invoke('save-progress', { lesson, data }),
   loadProgress: (lesson) => ipcRenderer.invoke('load-progress', lesson),
   getLessonData: (filePath) => ipcRenderer.invoke('get-lesson-data', filePath),
