1415dbededab34b76d4c7777b1db60de683d46ae
Drehon <filippo.monti66@gmail.com>
2025-07-27 20:02:47 +0200

    new errors

--- src/main.js
+++ src/main.js
@@ -51,7 +51,7 @@ const createWindow = () => {
     width: 1200,
     height: 800,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
+      preload: path.resolve(__dirname, 'preload.js'),
       nodeIntegration: true,
       contextIsolation: false,
     },
