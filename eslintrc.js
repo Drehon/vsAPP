4d8297d4aaf70bff60666659242d4b6afbd80476
Drehon <filippo.monti66@gmail.com>
2025-08-10 15:35:10 +0200

    issue persists

--- eslintrc.js
+++ eslintrc.js
@@ -9,6 +9,9 @@ module.exports = {
     node: true,
     jest: true,
   },
+  parserOptions: {
+    ecmaVersion: 2021,
+  },
   ignorePatterns: ["src/lib/**"],
   rules: {
     // Add any specific rule overrides here
